7bb46e6d0f7474accad16790671929d874c42f1b
James Kim <biojameskim2002@gmail.com>
2023-02-26 17:56:31 -0500

    Add hover effect to exec cards
    - Minor change to links/descriptions

--- src/pages/Fellowship.js
+++ src/pages/Fellowship.js
@@ -4,33 +4,33 @@ import urbana2 from '../assets/fellowship/urbana2.svg';
 import './css/Fellowship.css';
 import Footer from '../components/Footer';
 
-export default function Fellowship({isMobile=false}) {
-  return <div className="main-container">
-    <div className={`fellowship-container ${isMobile ? 'fellowship-container-mobile' : ''}`}>
-        <div className="fellowship-left">
-            <img className="urbana1" src={urbana1} alt="urbana1"></img>
-            <img className="urbana2" src={urbana2} alt="urbana2"></img>
-        </div>
-        <div className="fellowship-right">
-            <div className="text-container">
-                <h1>Campus Fellowships</h1>
-                <p>There are many other Christian campus fellowships like AAIV at Cornell, and we work with them to advance the Gospel to reach every corner of campus and glorify God. Feel free to check them out!</p>
+export default function Fellowship({ isMobile = false }) {
+    return <div className="main-container">
+        <div className={`fellowship-container ${isMobile ? 'fellowship-container-mobile' : ''}`}>
+            <div className="fellowship-left">
+                <img className="urbana1" src={urbana1} alt="urbana1"></img>
+                <img className="urbana2" src={urbana2} alt="urbana2"></img>
+            </div>
+            <div className="fellowship-right">
+                <div className="text-container">
+                    <h1>Campus Fellowships</h1>
+                    <p>There are many other Christian campus fellowships like AAIV at Cornell, and we work with them to advance the Gospel to reach every corner of campus and glorify God. Feel free to check them out!</p>
+                </div>
             </div>
         </div>
-    </div>
-    <div className="fellowship-section">
-      <div className="text-container">
-          <h1>Find a Fellowship that's Right for You</h1><br></br>
-          <a href='https://bethanycampuschurch.org/' target="_blank" rel="noopener noreferrer"><p>Bethany Christian Campus Church</p></a>
-          <a href='https://www.christianunion.org/ministries/universities/cornell' target="_blank" rel="noopener noreferrer"><p>Christian Union</p></a>
-          <a href='https://cbscornell.com/' target="_blank" rel="noopener noreferrer"><p>Chinese Bible Study</p></a>
-          <a href='https://www.crucornell.com/' target="_blank" rel="noopener noreferrer"><p>CRU</p></a>
-          <a href='https://cornellicf.weebly.com/' target="_blank" rel="noopener noreferrer"><p>Cornell International Christian Fellowship</p></a>
-          <a href='https://www.emmausroadcornell.com/' target="_blank" rel="noopener noreferrer"><p>Emmaus Road/Korean Church at Cornell</p></a>
-          <a href='https://www.instagram.com/p/CnfbHvMulWN/?hl=en' target="_blank" rel="noopener noreferrer"><p>Fellowship of Christian Athletes</p></a>
-          <a href='https://graduatechristianfellowship.wordpress.com/' target="_blank" rel="noopener noreferrer"><p>Graduate Christian Fellowship</p></a>
-      </div>
-    </div>
-    <Footer/>
-</div>;
+        <div className="fellowship-section">
+            <div className="text-container">
+                <h1>Find a Fellowship that's Right for You</h1><br></br>
+                <a href='https://bethanycampuschurch.org/' target="_blank" rel="noopener noreferrer"><p>Bethany Christian Campus Church</p></a>
+                <a href='https://www.christianunion.org/ministries/universities/cornell' target="_blank" rel="noopener noreferrer"><p>Christian Union</p></a>
+                <a href='https://cbscornell.com/' target="_blank" rel="noopener noreferrer"><p>Chinese Bible Study</p></a>
+                <a href='https://www.crucornell.com/' target="_blank" rel="noopener noreferrer"><p>CRU</p></a>
+                <a href='https://cornellicf.weebly.com/' target="_blank" rel="noopener noreferrer"><p>Cornell International Christian Fellowship</p></a>
+                <a href='https://www.emmausroadcornell.com/' target="_blank" rel="noopener noreferrer"><p>Emmaus Road/Korean Church at Cornell</p></a>
+                <a href='https://www.instagram.com/fca_cornell/' target="_blank" rel="noopener noreferrer"><p>Fellowship of Christian Athletes</p></a>
+                <a href='https://graduatechristianfellowship.wordpress.com/' target="_blank" rel="noopener noreferrer"><p>Graduate Christian Fellowship</p></a>
+            </div>
+        </div>
+        <Footer />
+    </div>;
 }
--- src/pages/SmallGroup.js
+++ src/pages/SmallGroup.js
@@ -1,4 +1,4 @@
-import React, {useRef, useState, useEffect} from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import './css/SmallGroup.css';
 import sg_img from '../assets/small_group/sg-img.svg';
 import monday_img from '../assets/small_group/elisabeth-melody.svg';
@@ -7,7 +7,7 @@ import tuesday_img from '../assets/small_group/grace-chen.svg';
 import thurs_img from '../assets/small_group/grace-li.svg';
 import Footer from '../components/Footer';
 
-export default function SmallGroup({isMobile=false}) {
+export default function SmallGroup({ isMobile = false }) {
   const [isVisible1, setVisible1] = useState(false);
   const [isVisible2, setVisible2] = useState(false);
   const [isVisible3, setVisible3] = useState(false);
@@ -18,69 +18,69 @@ export default function SmallGroup({isMobile=false}) {
   const thursdayRef = useRef();
   useEffect(() => {
     const observer1 = new IntersectionObserver(entries => {
-        entries.forEach(entry => setVisible1(entry.isIntersecting));
-      });
+      entries.forEach(entry => setVisible1(entry.isIntersecting));
+    });
     observer1.observe(mondayRef.current);
 
     const observer2 = new IntersectionObserver(entries => {
-        entries.forEach(entry => setVisible2(entry.isIntersecting));
-      });
+      entries.forEach(entry => setVisible2(entry.isIntersecting));
+    });
     observer2.observe(tuesdayRef.current);
 
     const observer3 = new IntersectionObserver(entries => {
-        entries.forEach(entry => setVisible3(entry.isIntersecting));
-      });
+      entries.forEach(entry => setVisible3(entry.isIntersecting));
+    });
     observer3.observe(wednesdayRef.current);
 
     const observer4 = new IntersectionObserver(entries => {
-        entries.forEach(entry => setVisible4(entry.isIntersecting));
-      });
+      entries.forEach(entry => setVisible4(entry.isIntersecting));
+    });
     observer4.observe(thursdayRef.current);
   }, []);
   return <div className="main-container">
     <div className={`sg-title-container ${isMobile ? 'sg-title-container-mobile' : ''}`}>
-        <img className="sg-title-image" src={sg_img} alt="sg-title"></img>
-        <div className="text-container">
-            <h1>Join a Small Group!</h1>
-            <p>This is the best way to get connected in a small and close, tight-knit community of fellow AAIVers. Our God is a personal God, and Small Group is one of the spaces in AAIV where you can experience God’s closeness, goodness, and unfailing love for yourself. </p>
-        </div>
+      <img className="sg-title-image" src={sg_img} alt="sg-title"></img>
+      <div className="text-container">
+        <h1>Join a Small Group!</h1>
+        <p>This is the best way to get connected in a small and close, tight-knit community of fellow AAIVers. Our God is a personal God, and Small Group is one of the spaces in AAIV where you can experience God’s closeness, goodness, and unfailing love for yourself. </p>
+      </div>
     </div>
     <div className="sg-leaders-section">
-        <div className="text-container">
-            <h1>Meet our Spring 2023 Small Group Leaders</h1>
-            <p>Guided by one or two of our amazing and friendly small group leaders, you can share life and explore God together in weekly meetings centered around a Bible study, which are often preceded by a small group dinner. Whether you’ve grown up in church your whole life or are just starting to question if there could be a God who has created all things, you are invited to come check out a small group as we cover the book of Hebrews! Learn more about the specific small groups listed below. </p>
+      <div className="text-container">
+        <h1>Meet our Spring 2023 Small Group Leaders</h1>
+        <p>Guided by one or two of our amazing and friendly small group leaders, you can share life and explore God together in weekly meetings centered around a Bible study, which are often preceded by a small group dinner. Whether you’ve grown up in church your whole life or are just starting to question if there could be a God who has created all things, you are invited to come check out a small group as we cover the book of Hebrews! Learn more about the specific small groups listed below. </p>
+      </div>
+      <div className='leaders'>
+        <div className={`leader fade-in-section ${isVisible1 ? 'is-visible' : ''}`} ref={mondayRef}>
+          <p>Monday</p>
+          <hr></hr>
+          <img src={monday_img} alt="monday"></img>
+          <h3 style={{ margin: '10px -2px 5px -2px' }}>Elisabeth & Melody (ep438, myx2)</h3>
+          <p>6pm @ URIS HALL 398</p>
         </div>
-        <div className='leaders'>
-            <div className={`leader fade-in-section ${isVisible1 ? 'is-visible' : ''}`} ref={mondayRef}>
-                <p>Monday</p>
-                <hr></hr>
-                <img src={monday_img} alt="monday"></img>
-                <h3 style={{margin: '10px -2px 5px -2px'}}>Elisabeth, Melody (ep438, myx2)</h3>
-                <p>6pm @ URIS HALL 398</p>
-            </div>
-            <div className={`leader fade-in-section ${isVisible2 ? 'is-visible' : ''}`} ref={tuesdayRef}>
-                <p>Tuesday</p>
-                <hr></hr>
-                <img src={tuesday_img} alt="tuesday"></img>
-                <h3>Grace Chen (glc84)</h3>
-                <p>7pm @ URIS HALL 204</p>
-            </div>
-            <div className={`leader fade-in-section ${isVisible3 ? 'is-visible' : ''}`} ref={wednesdayRef}>
-                <p>Wednesday</p>
-                <hr></hr>
-                <img src={monday_img} alt="wednesday"></img>
-                <h3>Simon & Jed (sc2925, jyt8)</h3>
-                <p>6pm @ HOLLISTER HALL</p>
-            </div>
-            <div className={`leader fade-in-section ${isVisible4 ? 'is-visible' : ''}`} ref={thursdayRef}>
-                <p>Thursday</p>
-                <hr></hr>
-                <img src={thurs_img} alt="thursday"></img>
-                <h3>Grace Li (gl443)</h3>
-                <p>7pm @ HIGHLANDS</p>
-            </div>
+        <div className={`leader fade-in-section ${isVisible2 ? 'is-visible' : ''}`} ref={tuesdayRef}>
+          <p>Tuesday</p>
+          <hr></hr>
+          <img src={tuesday_img} alt="tuesday"></img>
+          <h3>Grace Chen (glc84)</h3>
+          <p>7pm @ URIS HALL 204</p>
         </div>
+        <div className={`leader fade-in-section ${isVisible3 ? 'is-visible' : ''}`} ref={wednesdayRef}>
+          <p>Wednesday</p>
+          <hr></hr>
+          <img src={monday_img} alt="wednesday"></img>
+          <h3>Simon & Jed (sc2925, jyt8)</h3>
+          <p>6pm @ HOLLISTER HALL</p>
+        </div>
+        <div className={`leader fade-in-section ${isVisible4 ? 'is-visible' : ''}`} ref={thursdayRef}>
+          <p>Thursday</p>
+          <hr></hr>
+          <img src={thurs_img} alt="thursday"></img>
+          <h3>Grace Li (gl443)</h3>
+          <p>7pm @ HIGHLANDS</p>
+        </div>
+      </div>
     </div>
-    <Footer/>
+    <Footer />
   </div>;
 }
