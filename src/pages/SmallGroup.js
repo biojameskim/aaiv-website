ef8bef294ec51c0f3a81cc72887d01859407b60c
brookeye <by239@cornell.edu>
2025-01-29 13:58:46 -0500

    Updated sg profiles and pics

--- src/pages/SmallGroup.js
+++ src/pages/SmallGroup.js
@@ -8,8 +8,10 @@ import './css/SmallGroup.css';
 import sg_img from '../assets/small_group/sg-background.png';
 
 // These are the leader COVER photos for each small group leader.
-import mon_img from '../assets/small_group/FA24_Leaders/melody_img.svg';
-import wed_img from '../assets/small_group/FA24_Leaders/guillaume_img.svg'
+import mon_img from '../assets/small_group/SP25_Leaders/melody_img.svg';
+import tues_img from '../assets/small_group/SP25_Leaders/lizzy+elaine_img.svg';
+import wed_img from '../assets/small_group/SP25_Leaders/guillaume_img.svg';
+import thurs_img from '../assets/small_group/SP25_Leaders/jed_img.svg'
 
 // These are the group photos for each small group. 
 // import mon_sg from '../assets/small_group/melisabeth_sg.jpeg'
@@ -18,8 +20,11 @@ import wed_img from '../assets/small_group/FA24_Leaders/guillaume_img.svg'
 // import thurs_sg from '../assets/small_group/grace_li_sg.jpeg'
 
 // These are the profile photos for each small group leader (The circular pfps within the modal).
-import mon_prof from '../assets/small_group/FA24_Leaders/melody-prof.svg';
-import wed_prof from '../assets/small_group/FA24_Leaders/guillaume-prof.svg';
+import mon_prof from '../assets/small_group/SP25_Leaders/melody_prof.svg';
+import tues_prof from '../assets/small_group/SP25_Leaders/lizzy+elaine_prof.svg';
+import wed_prof from '../assets/small_group/SP25_Leaders/guillaume_prof.svg';
+import thurs_prof from '../assets/small_group/SP25_Leaders/jed_prof.svg'
+
 
 export default function SmallGroup({ isMobile = false }) {
   const customStyles = {
@@ -42,18 +47,18 @@ export default function SmallGroup({ isMobile = false }) {
 
 
   const [isVisible1, setVisible1] = useState(false);
-  // const [isVisible2, setVisible2] = useState(false);
+  const [isVisible2, setVisible2] = useState(false);
   const [isVisible3, setVisible3] = useState(false);
-  // const [isVisible4, setVisible4] = useState(false);
+  const [isVisible4, setVisible4] = useState(false);
   const [monIsOpen, setMonIsOpen] = useState(false);
-  // const [tuesIsOpen, setTuesIsOpen] = useState(false);
+  const [tuesIsOpen, setTuesIsOpen] = useState(false);
   const [wedIsOpen, setWedIsOpen] = useState(false);
-  // const [thursIsOpen, setThursIsOpen] = useState(false);
+  const [thursIsOpen, setThursIsOpen] = useState(false);
 
   const mondayRef = useRef();
-  // const tuesdayRef = useRef();
+  const tuesdayRef = useRef();
   const wednesdayRef = useRef();
-  // const thursdayRef = useRef();
+  const thursdayRef = useRef();
 
   useEffect(() => {
     const observer1 = new IntersectionObserver(entries => {
@@ -67,16 +72,16 @@ export default function SmallGroup({ isMobile = false }) {
     });
     observer1.observe(mondayRef.current);
 
-    // const observer2 = new IntersectionObserver(entries => {
-    //   entries.forEach(entry => {
-    //     if (entry.boundingClientRect.top > 0) {
-    //       if (entry.isIntersecting) {
-    //         setVisible2(entry.isIntersecting);
-    //       }
-    //     }
-    //   });
-    // });
-    // observer2.observe(tuesdayRef.current);
+    const observer2 = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.boundingClientRect.top > 0) {
+          if (entry.isIntersecting) {
+            setVisible2(entry.isIntersecting);
+          }
+        }
+      });
+    });
+    observer2.observe(tuesdayRef.current);
 
     const observer3 = new IntersectionObserver(entries => {
       entries.forEach(entry => {
@@ -89,16 +94,16 @@ export default function SmallGroup({ isMobile = false }) {
     });
     observer3.observe(wednesdayRef.current);
 
-    // const observer4 = new IntersectionObserver(entries => {
-    //   entries.forEach(entry => {
-    //     if (entry.boundingClientRect.top > 0) {
-    //       if (entry.isIntersecting) {
-    //         setVisible4(entry.isIntersecting);
-    //       }
-    //     }
-    //   });
-    // });
-    // observer4.observe(thursdayRef.current);
+    const observer4 = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.boundingClientRect.top > 0) {
+          if (entry.isIntersecting) {
+            setVisible4(entry.isIntersecting);
+          }
+        }
+      });
+    });
+    observer4.observe(thursdayRef.current);
   }, []);
 
   return <div className="main-container">
@@ -111,8 +116,8 @@ export default function SmallGroup({ isMobile = false }) {
     </div>
     <div className={`sg-leaders-section ${isMobile ? 'sg-leaders-section-mobile' : ''}`}>
       <div className="text-container">
-        <h1>Meet our Fall 2024 Small Group Leaders</h1>
-        <p>Guided by one or two of our amazing and friendly small group leaders, you can share life and explore God together in weekly meetings centered around a Bible study, which are often preceded by a small group dinner. Whether you’ve grown up in church your whole life or are just starting to question if there could be a God who has created all things, you are invited to come check out a small group as we cover the book of 1 Corinthians! Click on a small group leader to learn more!</p>
+        <h1>Meet our Spring 2025 Small Group Leaders</h1>
+        <p>Guided by one or two of our amazing and friendly small group leaders, you can share life and explore God together in weekly meetings centered around a Bible study, which are often preceded by a small group dinner. Whether you’ve grown up in church your whole life or are just starting to question if there could be a God who has created all things, you are invited to come check out a small group as we cover the book of Jonah! Click on a small group leader to learn more!</p>
       </div>
       <div className='leaders'>
         <div className={`leader fade-in-section ${isVisible1 ? 'is-visible' : ''}`} ref={mondayRef} onClick={() => { setMonIsOpen(true) }}>
@@ -125,20 +130,20 @@ export default function SmallGroup({ isMobile = false }) {
               <img className="sg-icon" src={email} alt="email-icon"></img>
             </a>
           </div>
-          <p>7:30pm @ Uris Hall 262</p>
+          <p>7:30pm @ Uris Hall 260</p>
         </div>
-        {/* <div className={`leader fade-in-section ${isVisible2 ? 'is-visible' : ''}`} ref={tuesdayRef} onClick={() => { setTuesIsOpen(true) }}>
+        <div className={`leader fade-in-section ${isVisible2 ? 'is-visible' : ''}`} ref={tuesdayRef} onClick={() => { setTuesIsOpen(true) }}>
           <p>Tuesday</p>
           <hr></hr>
           <img src={tues_img} alt="tuesday"></img>
           <div className="sg-title">
-            <h3>Ro-Ann Shen & Karina Song</h3>
-            <a href="mailto: rs2545@cornell.edu">
+            <h3>Elaine Tsai & Lizzy Zhang</h3>
+            <a href="mailto: ect65@cornell.edu">
               <img className="sg-icon" src={email} alt="email-icon"></img>
             </a>
           </div>
-          <p>6:30pm @ Uris 204</p>
-        </div> */}
+          <p>6:30pm @ Stimson 206</p>
+        </div>
         <div className={`leader fade-in-section ${isVisible3 ? 'is-visible' : ''}`} ref={wednesdayRef} onClick={() => { setWedIsOpen(true) }}>
           <p>Wednesday</p>
           <hr></hr>
@@ -149,9 +154,9 @@ export default function SmallGroup({ isMobile = false }) {
               <img className="sg-icon" src={email} alt="email-icon"></img>
             </a>
           </div>
-          <p>7:30pm @ Stimson 206</p>
+          <p>7:00pm @ Uris 204</p>
         </div>
-        {/* <div className={`leader fade-in-section ${isVisible4 ? 'is-visible' : ''}`} ref={thursdayRef} onClick={() => { setThursIsOpen(true) }}>
+        <div className={`leader fade-in-section ${isVisible4 ? 'is-visible' : ''}`} ref={thursdayRef} onClick={() => { setThursIsOpen(true) }}>
           <p>Thursday</p>
           <hr></hr>
           <img src={thurs_img} alt="thursday"></img>
@@ -161,8 +166,8 @@ export default function SmallGroup({ isMobile = false }) {
               <img className="sg-icon" src={email} alt="email-icon"></img>
             </a>
           </div>
-          <p>7:30pm @ Hollister Hall 401</p>
-        </div> */}
+          <p>6:00pm @ CHOP ~ 536 thurston ave</p>
+        </div>
       </div>
       <div className="sg-modals">
         <Modal
@@ -171,7 +176,7 @@ export default function SmallGroup({ isMobile = false }) {
           style={customStyles}
         >
           <div>
-            <Profile image={mon_prof} title={'Melody Xu'} text={'Mondays 7:30pm @ Uris Hall 262 (bring-your-own-dinner @ 7pm)'} email={'myx2@cornell.edu'} />
+            <Profile image={mon_prof} title={'Melody Xu'} text={'Mondays 7:30pm @ Uris Hall 260 (bring-your-own-dinner @ 7pm)'} email={'myx2@cornell.edu'} />
             <Question
               isMobile={isMobile}
               image={mon_prof}
@@ -180,8 +185,8 @@ export default function SmallGroup({ isMobile = false }) {
                 <div className="sg-answer">
                   <span><span style={{ fontWeight: 500 }}>Major:</span> HBHS '25</span>
                   <span><span style={{ fontWeight: 500 }}>Hometown:</span> Cincinnati, OH & Shanghai, China</span>
-                  <span><span style={{ fontWeight: 500 }}>Fav lunch spot:</span> Goldies</span>
-                  <span><span style={{ fontWeight: 500 }}>Best study spot:</span> Olin 7th floor stacks</span>
+                  {/* <span><span style={{ fontWeight: 500 }}>Fav lunch spot:</span> Goldies</span>
+                  <span><span style={{ fontWeight: 500 }}>Best study spot:</span> Olin 7th floor stacks</span> */}
                 </div>
               }
             />
@@ -189,38 +194,38 @@ export default function SmallGroup({ isMobile = false }) {
               isMobile={isMobile}
               image={mon_prof}
               question={"Favorite AAIV Memory?"}
-              answer={"Playing Nertz at algae with alumni"}
+              answer={"Sledding down the slope during Sabbath"}
             />
             <Question
               isMobile={isMobile}
               image={mon_prof}
-              question={"Vision for small group?"}
-              answer={"For me, AAIV small group is a safe place to be vulnerable, share our doubts, struggles, and joys. Through studying scripture, we aim to get to know Jesus better and find joy in our faith journey with one another"}
+              question={"Prayer for small group?"}
+              answer={"That people will enjoy small group and remember what they read about!"}
             />
             {/* <SgPhoto src={melisabeth_sg} alt="melisabeth small group" /> */}
           </div>
         </Modal>
-        {/* <Modal
+        <Modal
           isOpen={tuesIsOpen}
           onRequestClose={() => { setTuesIsOpen(false) }}
           style={customStyles}
         >
           <div>
-            <Profile image={tues_prof} title={'Ro-Ann Shen & Karina Song'} text={'Tuesdays 6:30pm @ Uris 204'} email={'rs2545@cornell.edu'} />
+            <Profile image={tues_prof} title={'Elaine Tsai & Lizzy Zhang'} text={'Tuesdays 6:30pm @ Stimson 206 (Dinner @ Okies at 5:30PM 😋)'} email={'ect65@cornell.edu'} />
             <Question
               isMobile={isMobile}
-              image={karina_prof} //Kaitlyn will be for Karina
+              image={tues_prof} //Kaitlyn will be for Karina
               question={"Fast Facts"}
               answer={
                 <div className="sg-answer">
-                  <span><span style={{ fontWeight: 500 }}>Major:</span> ILR, minoring in Business, Inequality Studies, Theatre '24</span>
-                  <span><span style={{ fontWeight: 500 }}>Hometown:</span> Seoul, South Korea</span>
-                  <span><span style={{ fontWeight: 500 }}>Fav lunch spot:</span> Bus Stop Bagels</span>
-                  <span><span style={{ fontWeight: 500 }}>Best study spot:</span> Catherwood Library</span>
+                  <span><span style={{ fontWeight: 500 }}>Major:</span> Elaine (Computer Science '26), Lizzy (Animal Sciences '26)</span>
+                  <span><span style={{ fontWeight: 500 }}>Hometown:</span> Elaine (Taipei, Taiwan), Lizzy (San Jose, CA)</span>
+                  {/* <span><span style={{ fontWeight: 500 }}>Fav lunch spot:</span> Bus Stop Bagels</span>
+                  <span><span style={{ fontWeight: 500 }}>Best study spot:</span> Catherwood Library</span> */}
                 </div>
               }
             />
-            <Question
+            {/* <Question
               isMobile={isMobile}
               image={roann_prof} //Angie will be for Ro-Ann
               question={"Fast Facts"}
@@ -232,28 +237,28 @@ export default function SmallGroup({ isMobile = false }) {
                   <span><span style={{ fontWeight: 500 }}>Best study spot:</span> Libe Cafe by the windows :D </span>
                 </div>
               }
-            />
+            /> */}
             <Question
               isMobile={isMobile}
-              image={karina_prof}
+              image={tues_prof}
               question={"Favorite AAIV Memory?"}
-              answer={"Sunrising with Arvin, Agnes, and Sam"}
+              answer={"Elaine (Prepping boys gen and girls gen  💃🕺), Lizzy (Just dance and karaoke after large group💃)"}
             />
             <Question
               isMobile={isMobile}
-              image={roann_prof}
-              question={"Vision for small group?"}
-              answer={"Our vision is to have small group as a supportive, non-intimidating space for anyone regardless of spiritual standing to study scripture and build spiritual discipline. We hope to foster family energy through community activites and a space where people feel safe to be open and vulnerable."}
+              image={tues_prof}
+              question={"Prayer for small group?"}
+              answer={"Living out Christ’s love to one another and forming close friendships! "}
             />
           </div>
-        </Modal> */}
+        </Modal>
         <Modal
           isOpen={wedIsOpen}
           onRequestClose={() => { setWedIsOpen(false) }}
           style={customStyles}
         >
           <div>
-            <Profile image={wed_prof} title={'Guillaume Ah-Hot'} text={'Wednesdays 7:30pm @ Stimson 206 (Dinner 6:30pm @ Okenshields)'} netid={'gaa59'} email={'gaa59@cornell.edu'} />
+            <Profile image={wed_prof} title={'Guillaume Ah-Hot'} text={'Wednesdays 7:00pm @ Uris 204'} netid={'gaa59'} email={'gaa59@cornell.edu'} />
             <Question
               isMobile={isMobile}
               image={wed_prof}
@@ -261,9 +266,9 @@ export default function SmallGroup({ isMobile = false }) {
               answer={
                 <div className="sg-answer">
                   <span><span style={{ fontWeight: 500 }}>Major:</span> ECE '27</span>
-                  <span><span style={{ fontWeight: 500 }}>Hometown:</span> Singapore</span>
-                  <span><span style={{ fontWeight: 500 }}>Fav lunch spot:</span> Okies after 2PM</span>
-                  <span><span style={{ fontWeight: 500 }}>Best study spot:</span> Kirby Reading Room</span>
+                  <span><span style={{ fontWeight: 500 }}>Hometown:</span> Paris</span>
+                  {/* <span><span style={{ fontWeight: 500 }}>Fav lunch spot:</span> Okies after 2PM</span>
+                  <span><span style={{ fontWeight: 500 }}>Best study spot:</span> Kirby Reading Room</span> */}
                 </div>
               }
             />
@@ -271,24 +276,24 @@ export default function SmallGroup({ isMobile = false }) {
               isMobile={isMobile}
               image={wed_prof}
               question={"Favorite AAIV memory?"}
-              answer={"Selah (AAIV Fall Retreat)"}
+              answer={"Wednesday Small Group last semester so you should totally come to wednesday small group this semester!"}
             />
             <Question
               isMobile={isMobile}
               image={wed_prof}
-              question={"Vision for small group?"}
-              answer={"I want to create an intimate space where anyone is able to make friends while also learning about God and His Word."}
+              question={"Prayer for small group?"}
+              answer={"Mutual edification (˶ᵔ ᵕ ᵔ˶)"}
             />
             {/* <SgPhoto src={wed_sg} alt="Cassy small group" /> */}
           </div>
         </Modal>
-        {/* <Modal
+        <Modal
           isOpen={thursIsOpen}
           onRequestClose={() => { setThursIsOpen(false) }}
           style={customStyles}
         >
           <div>
-            <Profile image={thurs_prof} title={'Jed Tan'} text={'Thursdays 7:30pm @ Hollister Hall'} netid={'jyt8'} email={'jyt8@cornell.edu'} />
+            <Profile image={thurs_prof} title={'Jed Tan'} text={'Thursdays 6:00pm @ CHOP ~ 536 Thurston Ave (Morrison dinner @ 5PM)'} netid={'jyt8'} email={'jyt8@cornell.edu'} />
             <Question
               isMobile={isMobile}
               image={thurs_prof}
@@ -297,8 +302,8 @@ export default function SmallGroup({ isMobile = false }) {
                 <div className="sg-answer">
                   <span><span style={{ fontWeight: 500 }}>Major:</span> Civil Engineering '25</span>
                   <span><span style={{ fontWeight: 500 }}>Hometown:</span> Stony Brook, NY</span>
-                  <span><span style={{ fontWeight: 500 }}>Fav lunch spot:</span> Risley's Nashville Hot Chicken</span>
-                  <span><span style={{ fontWeight: 500 }}>Best study spot:</span> Sage Atrium</span>
+                  {/* <span><span style={{ fontWeight: 500 }}>Fav lunch spot:</span> Risley's Nashville Hot Chicken</span>
+                  <span><span style={{ fontWeight: 500 }}>Best study spot:</span> Sage Atrium</span> */}
                 </div>
               }
             />
@@ -306,16 +311,16 @@ export default function SmallGroup({ isMobile = false }) {
               isMobile={isMobile}
               image={thurs_prof}
               question={"Favorite AAIV memory?"}
-              answer={"Early morning devos and 6 mile jog at Basileia"}
+              answer={"Small group picnics and movie nights"}
             />
             <Question
               isMobile={isMobile}
               image={thurs_prof}
-              question={"Vision for small group?"}
-              answer={"A place to learn from each other what God's word means, and how to live lives that imitate Christ's example, caring deeply for one another."}
+              question={"Prayer for small group?"}
+              answer={"Love for learning from scripture."}
             />
           </div>
-        </Modal> */}
+        </Modal>
       </div>
     </div>
     <Footer />
