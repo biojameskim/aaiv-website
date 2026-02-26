6f2d1b832ebc9b0f86892aed9d4185d8a8765402
brookeye <by239@cornell.edu>
2026-02-26 11:55:18 -0500

    Update vision statment

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -61,13 +61,21 @@ export default function Home({ isMobile = false }) {
             <div className="text-container">
                 <h1>Our Vision</h1>
                 <p>
-                Our vision at AAIV is to establish a witnessing, Christ-centered community at Cornell. This year, we hope to grow deeper in our love for God as we unite Cornell's campus and His people. 
+                Our vision at AAIV is to establish a witnessing, Christ-centered community at Cornell. This year, we hope to grow deeper in our love for God through one heart, one body, and one campus for Christ. 
                 </p>
                 <p>
                 We strive to be a beacon of light on campus, living fully for God in all that we do. By grounding ourselves in Scripture and experiencing His presence, love, and guidance, we hope to build one another up and share this joy with those around us.
                 </p>
                 <p>
                 Through intentional discipleship, we hope to cultivate a heart that faithfully serves and glorifies God, leading others to know and follow Christ.
+                </p>
+                <p>
+                Through large group, we strive to LOVE and pursue God wholeheartedly and deeply. Through small group, we hope to LOVE and build one another up to be like Jesus. For our community, we will LOVE and live our our faith BODLY, bearing faithful witness to Christ.
+                </p>
+                <p>
+                Ephesians 4:4-6
+
+                (4) There is one body and one Spirit, just as you were called to one hope when you were called; (5) one Lord, one faith, one baptism; (6) one God and Father of all, who is over all and through all and in all.
                 </p>
             </div>
             <img className="vision-image" src={vision_image} alt="vision"></img>
